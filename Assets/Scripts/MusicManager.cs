987bf6d8fe160ad9861e0034cfeccd27263a907f
David Belcher <davidjamesbelcher0@gmail.com>
2020-09-21 19:05:51 +1000

    Add audio source to play intro and normal BG music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = IntroMusic;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!IntroOver && Time.timeSinceLevelLoad > IntroLength)
+        {
+            audioSource.clip = NormalBGMusic;
+            audioSource.Play();
+            IntroOver = true;
+        }
+    }
+
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip IntroMusic;
+
+    private float IntroLength = 4.2f;
+
+    [SerializeField]
+    private AudioClip NormalBGMusic;
+
+    private bool IntroOver = false;
+}
